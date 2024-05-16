90a83cff97d6006d1ea0b11c7430b51effd82cb4
Arun sd <146158684+Arun1SD@users.noreply.github.com>
2024-05-16 13:00:23 +0530

    logic

--- swapint.c
+++ swapint.c
@@ -4,13 +4,16 @@ int main()
     int a,b,temp;
     printf("enter the value of a = ");
     scanf("%d",&a);
+
     printf("enter the value of b = ");
     scanf("%d",&b);
 
     printf("\nbefore swap a = %d b = %d\n",a,b);
+    //logic
     temp=a;
     a=b;
     b=temp;
+
     printf("after swap a = %d b= %d",a,b);
 
     return 0;
